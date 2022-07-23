40a51ef899f34e1272270cb6774863a75a7fb95a
Azhar <abdazhar786@gmail.com>
2022-07-23 11:35:14 +0530

    Operators Basic

---
+++ operators.js
@@ -0,0 +1,103 @@
+// Arithmetic Operators
+
+// let x = 10;
+// let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+//console.log(x ** y);
+
+//Increament (++)
+
+// console.log(x++);
+// console.log(x);
+
+//Decrement (--)
+
+//console.log(--x);
+
+//Assignment Operators
+// let x = 10;
+
+// x = x + 5;
+// x += 5;
+// console.log(x);
+
+// x = x * 3;
+// x *= 3;
+
+// Comparision Operators
+
+//let x = 1;
+//Relational
+// console.log(x > 0);
+// console.log(x >= 1);
+// console.log(x < 1);
+// console.log(x <= 1);
+
+//Equality
+// console.log(x === 1);
+// console.log(x !== 1);
+
+//Equality Operators
+
+//Strict Equality (Type + Value)
+// console.log(1 === 1);
+// console.log('1' === 1);
+
+//Lose Equality (Value)
+// console.log(1 == 1);
+// console.log('1' == 1);
+//console.log(true == 1);
+
+//ternary Operator
+
+//If a customer has more than 100 points, they are 'gold' customer, otherwise, they are a 'silver' customer
+
+let points = 110;
+let type = points < 100 ? "gold" : "silver";
+
+console.log(type);
+
+//Logical Operators
+//logical AND (&&)
+//Return TRUE if both operands are TRUE
+//console.log(true && true);
+
+//Logical OR (||)
+//Returns TRUE if one of the operands is TRUE
+// let highIncome = false;
+// let goodCreditScore = false;
+// let eligibleForLoan = highIncome || goodCreditScore;
+// console.log("Eligible", eligibleForLoan);
+
+// //NOT (!)
+// let applicationRefused = !eligibleForLoan;
+// console.log("Application Refused", applicationRefused);
+
+// Quiz 1
+
+//q3
+// let x = 10;
+// let y = x > 10 ? 1 : 0;
+
+// console.log(y);
+
+//q4
+//let x = (2 + 3) * (4 + 5);
+//console.log(x);
+
+//Exercise swap colors
+
+// let a = "red";
+// let b = "blue";
+
+// let c = a;
+// a = b;
+// b = c;
+
+//console.log(a);
+//console.log(b);
