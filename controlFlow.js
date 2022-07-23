f334a6d78d171b3f871bd45553136a043263c517
Azhar <abdazhar786@gmail.com>
2022-07-23 13:54:13 +0530

    Practice basic question

--- controlFlow.js
+++ controlFlow.js
@@ -10,25 +10,3 @@ let hour = 16;
 if (hour >= 6 && hour < 12) console.log("Good morning");
 else if (hour >= 12 && hour < 18) console.log("Good afternoon");
 else console.log("Good evening");
-
-//SWITCH..CASE
-//let role = "guest";
-
-// switch (role) {
-//   case "guest":
-//     console.log("Guest User");
-//     break;
-
-//   case "moderator":
-//     console.log("Moderator User");
-//     break;
-
-//   default:
-//     console.log("Unknown User");
-// }
-
-//Using if else instead of switch statement
-
-// if (role === "guest") console.log("Guest User");
-// else if (role === "moderator") console.log("Moderator User");
-// else console.log("Unknown User");
